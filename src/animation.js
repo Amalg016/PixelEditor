baf616dfb5947de88e5d870bbb246c62f6fc668a
amalg016 <amalgjoseph1@gmail.com>
2025-08-26 01:10:07 +0530

    Fixed the Bug fix. changed to the ctx of the preview window to change color

--- src/animation.js
+++ src/animation.js
@@ -1,69 +1,69 @@
-let play=false;
+let play = false;
 let animInterval;
-let timegap=1000;
+let timegap = 1000;
 
-const PreviewCanvas=document.getElementById("preview-canvas");
-const timeInput=document.getElementById("preview-canvas-time-input");
-const previewCtx=PreviewCanvas.getContext("2d");
-let PreviewCanvasWidth=PreviewCanvas.width;
-let PreviewCanvasHeight=PreviewCanvas.height;
+const PreviewCanvas = document.getElementById("preview-canvas");
+const timeInput = document.getElementById("preview-canvas-time-input");
+const previewCtx = PreviewCanvas.getContext("2d");
+let PreviewCanvasWidth = PreviewCanvas.width;
+let PreviewCanvasHeight = PreviewCanvas.height;
 
-indexInput.value=0;
+indexInput.value = 0;
 
-function toggleAnim(e){
-    play=!play
-  if(play){
-    e.target.classList.remove("unselected");
-     animInterval=setInterval(NextImage,timegap);
-  }
-  else{
-    e.target.classList.add("unselected");
-    clearInterval(animInterval);
-    animInterval=null;
-  }
-  
+function toggleAnim(e) {
+    play = !play
+    if (play) {
+        e.target.classList.remove("unselected");
+        animInterval = setInterval(NextImage, timegap);
+    }
+    else {
+        e.target.classList.add("unselected");
+        clearInterval(animInterval);
+        animInterval = null;
+    }
+
 }
 // function changeIndex(e){
 //     timeInput.value;
 // }
 
-function changeTimeInterval(e){
-    timegap = timeInput.value*1000;
-    if(animInterval){
-        clearInterval(animInterval); 
-        animInterval=null;
+function changeTimeInterval(e) {
+    timegap = timeInput.value * 1000;
+    if (animInterval) {
+        clearInterval(animInterval);
+        animInterval = null;
     }
-    
-    if(play){
-        animInterval=setInterval(NextImage,timegap);
+
+    if (play) {
+        animInterval = setInterval(NextImage, timegap);
     }
 }
 
-function changeSlidermaxValue(n){
-    indexInput.max=n;
+function changeSlidermaxValue(n) {
+    indexInput.max = n;
 }
 
-function NextImage(){
-    previewCtx.clearRect(0,0,PreviewCanvasWidth,PreviewCanvasHeight);
+function NextImage() {
+    previewCtx.clearRect(0, 0, PreviewCanvasWidth, PreviewCanvasHeight);
     // const pCanvas=bottomBoximages[+indexInput.value];
-   // console.log(pCanvas,indexInput.value);
-   for(let x=0;x<Resolution.count;x++){
-    for(let y=0;y<Resolution.count;y++){
-       const colr= files[+indexInput.value][files[+indexInput.value].length-1][x][y];
-       ctx.fillStyle=colr;
-       if(colr!="none"){
-           const snapX=x*PreviewCanvasWidth/Resolution.count;
-           const snapY=y* PreviewCanvasHeight/Resolution.count;
-           previewCtx.fillRect(snapX, snapY, PreviewCanvasWidth/Resolution.count, PreviewCanvasHeight/Resolution.count);
-       } 
-       // else if(colr=="none"&&i==0){
-       else if(colr=="none"){
-           const snapX=x*PreviewCanvasWidth/Resolution.count;
-           const snapY=y*PreviewCanvasHeight/Resolution.count;
-           previewCtx.clearRect(snapX,snapY, PreviewCanvasWidth/Resolution.count,PreviewCanvasHeight/Resolution.count);
-       }
+    // console.log(pCanvas,indexInput.value);
+    for (let x = 0; x < Resolution.count; x++) {
+        for (let y = 0; y < Resolution.count; y++) {
+            const colr = files[+indexInput.value][files[+indexInput.value].length - 1][x][y];
+            previewCtx.fillStyle = colr;
+            if (colr != "none") {
+                const snapX = x * PreviewCanvasWidth / Resolution.count;
+                const snapY = y * PreviewCanvasHeight / Resolution.count;
+                previewCtx.fillRect(snapX, snapY, PreviewCanvasWidth / Resolution.count, PreviewCanvasHeight / Resolution.count);
+            }
+            // else if(colr=="none"&&i==0){
+            else if (colr == "none") {
+                const snapX = x * PreviewCanvasWidth / Resolution.count;
+                const snapY = y * PreviewCanvasHeight / Resolution.count;
+                previewCtx.clearRect(snapX, snapY, PreviewCanvasWidth / Resolution.count, PreviewCanvasHeight / Resolution.count);
+            }
+        }
     }
-   }
     // previewCtx.drawImage(pCanvas,0,0,pCanvas.width,pCanvas.height,0,0,PreviewCanvasWidth,PreviewCanvasHeight);
-    indexInput.value=(+indexInput.value+1)%bottomBoximages.length;
+    indexInput.value = (+indexInput.value + 1) % bottomBoximages.length;
 }
